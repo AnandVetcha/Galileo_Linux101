ecaaf0f4cf97f68c40730f931d88f10475ecb1b9
AnandVetcha <AnandVetcha@users.noreply.github.com>
2014-09-20 19:30:54 +0530

    Update SetGalileoIP.ino

--- GalileoIP/SetGalileoIP.ino
+++ GalileoIP/SetGalileoIP.ino
@@ -1,8 +1,8 @@
 void setup() {
   // Start Telnet server
   system("telnetd -l /bin/sh");
-  // Set IP to 169.253.1.1
-  system("ifconfig eth0 169.253.1.1 netmask 255.255.0.0 up");
+  // Set IP to 169.254.1.10
+  system("ifconfig eth0 169.254.1.10 netmask 255.255.0.0 up");
 }
 
 void loop() {
